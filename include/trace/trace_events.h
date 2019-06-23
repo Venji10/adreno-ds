468777f6bda76ecbbd193da1ac3b588f5c734ee9
Venji10 <38562746+Venji10@users.noreply.github.com>
2019-06-23 14:31:50 +0200

    Update trace_events.h

--- include/trace/trace_events.h
+++ include/trace/trace_events.h
@@ -137,7 +137,7 @@ TRACE_MAKE_SYSTEM_STR();
 #define TRACE_EVENT_PERF_PERM(name, expr...)				\
 	__TRACE_EVENT_PERF_PERM(name, expr)
 
-#include TRACE_INCLUDE(TRACE_INCLUDE_FILE)
+#include "../../drivers/media/platform/msm/camera/cam_utils/cam_trace.h"
 
 /*
  * Stage 2 of the trace events.
